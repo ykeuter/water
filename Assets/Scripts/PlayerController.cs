9d53c6b8781bd7410d129e2382a5e9402aec53b6
Yvo Keuter <ykeuter@me.com>
2020-06-10 08:57:48 +0200

    player control

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 5;
+    [SerializeField] float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, Input.GetAxis("Horizontal"));
+        transform.Rotate(Vector3.right, Input.GetAxis("Vertical"));
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += transform.forward * Time.deltaTime * speed;
+        }
+    }
+}
