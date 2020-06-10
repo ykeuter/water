38b9b3e50ce5c25d7e4379f63586339c7dbbb43b
Yvo Keuter <ykeuter@me.com>
2020-06-10 21:36:13 +0200

    added game play

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    float score = 0;
+    float startTime = 10;
+    float timeLeft;
+    float bound = 30;
+    int maxBombs = 10;
+    int numBombs = 0;
+    public bool isGameActive = false;
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] TextMeshProUGUI timeText;
+    [SerializeField] GameObject gameOverScreen;
+    [SerializeField] GameObject titleScreen;
+    [SerializeField] GameObject bombPrefab;
+    [SerializeField] GameObject powerupPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +30,60 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isGameActive)
+        {
+            timeText.text = "Time: " + Mathf.Round(timeLeft);
+            if (timeLeft < 0)
+            {
+                GameOver();
+            }
+            timeLeft -= Time.deltaTime;
+
+        }
+    }
+
+    public void Powerup()
+    {
+        if (numBombs < maxBombs)
+        {
+            numBombs++;
+            Instantiate(bombPrefab, RandomSpawnPos(), bombPrefab.transform.rotation);
+        }
+        Instantiate(powerupPrefab, RandomSpawnPos(), bombPrefab.transform.rotation);
+        timeLeft = startTime;
+        score++;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverScreen.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame()
+    {
+        isGameActive = true;
+        score = -1;
+        Powerup();
+        titleScreen.SetActive(false);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        float x = Random.Range(0, bound);
+        float y = Random.Range(0, bound);
+        float z = Random.Range(0, bound);
+        return new Vector3(x, y, z);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,24 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float rotationSpeed = 5;
-    [SerializeField] float speed = 10;
+    [SerializeField] float rotationSpeed = 25;
+    [SerializeField] float speed = 30;
+    GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.up, Input.GetAxis("Horizontal"));
-        transform.Rotate(Vector3.right, Input.GetAxis("Vertical"));
-
-        if (Input.GetKey(KeyCode.Space))
+        if (gameManager.isGameActive)
         {
-            transform.position += transform.forward * Time.deltaTime * speed;
+            transform.Rotate(Vector3.up, Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed);
+            transform.Rotate(Vector3.right, Input.GetAxis("Vertical") * Time.deltaTime * rotationSpeed);
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                transform.position += transform.forward * Time.deltaTime * speed;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+        else
+        {
+            Destroy(other);
+            gameManager.Powerup();
         }
     }
 }
